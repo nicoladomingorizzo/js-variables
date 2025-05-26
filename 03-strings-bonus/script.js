2dfce6f3f119c58a28ffbd68bad71b636da034ab
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-26 17:48:24 +0200

    Fix city replacement in strings-bonus script

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -16,7 +16,7 @@ console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = ('York', 'New Delhi');
+const cityReplaced = city.replace('York', 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
